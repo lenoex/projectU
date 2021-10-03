c206445f21e38e001a86be4aaf1ff3abcdb06b99
RedX <projectdyred@gmail.com>
2021-10-03 16:46:51 +0300

    lessonV1

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-let a=1;
+"use strict";
+
+let number0FILMS = prompt('Сколько посмотрел фльмов?','');
+let lastFilm = prompt('Один из последних просмотренных фильмов?');
+let lastFilmCritik = prompt('На сколько оцените его?');
+let personalMovieDB = {
+	cout:number0FILMS,
+	movies:{
+	lastFilmCritik,lastFilm
+	},
+	actors:"",
+	genres:[],
+	privat:false,
+};
+console.log(personalMovieDB);
+
